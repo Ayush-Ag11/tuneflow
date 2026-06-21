0a2e99d94ec0574fc32488e5f74410dcf9dbab52
Ayush-Ag11 <ayushagnihotri22@gmail.com>
2026-06-21 06:21:09 +0530

    feat(music-service): add initial project structure with Maven wrapper, application properties, and basic Spring Boot application

---
+++ music-service/src/test/java/com/tuneflow/music_service/MusicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuneflow.music_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
